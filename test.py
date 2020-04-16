9edd342fe5cfa1fa2c9f79d35f57ef2e0411e18f
JaredButcher <jared.butcher1219@gmail.com>
2020-04-15 21:18:20 -0400

    potato

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import pygit2
 
 repo = pygit2.Repository(".")
-
-repo.index.add_all()
-repo.merge(repo.lookup_reference("FETCH_HEAD").target)
-print(len(repo.index.conflicts))
-print(repo.index.conflicts.keys)
-for thing in repo.index.conflicts:
-	print(thing)
+try:
+	repo.index.add_all()
+	repo.merge(repo.lookup_reference("FETCH_HEAD").target)
+	print(repo.index.conflicts.keys)
+	for thing in repo.index.conflicts:
+		print(thing)
+finally:
+	repo.state_cleanup()
