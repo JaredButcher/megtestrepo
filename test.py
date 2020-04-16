adffeb8523dbc02a3ee2be51e1dba63772bf826b
JaredButcher <jared.butcher1219@gmail.com>
2020-04-15 21:24:14 -0400

    potato

--- test.py
+++ test.py
@@ -6,5 +6,6 @@ try:
 	repo.merge(repo.lookup_reference("FETCH_HEAD").target)
 	for thing in repo.index.conflicts:
 		print(thing[0].path)
+		repo.index.add(thing[2])
 finally:
 	repo.state_cleanup()
