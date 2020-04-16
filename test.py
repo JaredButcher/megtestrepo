f698c3d13303e2fb3633912ba416e283239ee7d4
JaredButcher <jared.butcher1219@gmail.com>
2020-04-15 21:23:05 -0400

    potato

--- test.py
+++ test.py
@@ -4,8 +4,7 @@ repo = pygit2.Repository(".")
 try:
 	repo.index.add_all()
 	repo.merge(repo.lookup_reference("FETCH_HEAD").target)
-	print(repo.index.conflicts.keys)
 	for thing in repo.index.conflicts:
-		print(thing)
+		print(thing[0].path)
 finally:
 	repo.state_cleanup()
