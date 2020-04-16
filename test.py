8b7df48f290597b6a462b4bbb69e3917b68a9c2c
JaredButcher <jared.butcher1219@gmail.com>
2020-04-15 21:27:21 -0400

    potot

--- test.py
+++ test.py
@@ -5,7 +5,8 @@ try:
 	repo.index.add_all()
 	repo.merge(repo.lookup_reference("FETCH_HEAD").target)
 	for thing in repo.index.conflicts:
-		print(thing[0].path)
+		for thing2 in thing:
+			print(thing2.path)
 		repo.index.add(thing[2])
 	repo.index.write()
 	repo.create_commit('HEAD', repo.default_signature, repo.default_signature, "AHH43", repo.index.write_tree(), [repo.head.target])
